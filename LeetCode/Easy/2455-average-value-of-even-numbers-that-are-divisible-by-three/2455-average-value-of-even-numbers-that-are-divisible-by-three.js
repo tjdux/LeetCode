88f83634347bf3dbe6f7bb7574b4b319ca9f3c82
tjdux <119560046+tjdux@users.noreply.github.com>
2023-04-25 00:40:05 +0900

    Time: 67 ms (42.48%), Space: 43 MB (61.44%) - LeetHub

---
+++ LeetCode/Easy/2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var averageValue = function(nums) {
+    let sum = 0;
+    let count = 0;
+    
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] % 2 === 0 && nums[i] % 3 === 0){
+            sum += nums[i];
+            ++count;
+        }
+    }
+    
+    return count === 0 ? 0 : Math.floor(sum / count)
+};

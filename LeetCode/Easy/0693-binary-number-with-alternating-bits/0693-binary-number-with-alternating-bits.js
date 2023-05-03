bce39d4555920389d203726a83e42d2e0515d4a3
tjdux <119560046+tjdux@users.noreply.github.com>
2023-05-03 17:31:41 +0900

    Time: 58 ms (66.37%), Space: 41.3 MB (97.37%) - LeetHub

---
+++ LeetCode/Easy/0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var hasAlternatingBits = function(n) {
+    const binary = n.toString(2);
+    for (let i = 0; i < binary.length - 1; i++){
+        if (binary[i] === binary[i+1]) return false
+    }
+    return true
+};
